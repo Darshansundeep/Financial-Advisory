9b0227491d14a58923180b6448a0a79258c13a8f
darshansundeep <sangarajudarshan@gmail.com>
2024-11-14 16:25:17 +0400

    Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 // pages/_app.js
 import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="app-container">
-            <Component {...pageProps} />
+      <Component {...pageProps} />
+      <Analytics /> {/* This enables Vercel Analytics */}
     </div>
   );
 }
+
